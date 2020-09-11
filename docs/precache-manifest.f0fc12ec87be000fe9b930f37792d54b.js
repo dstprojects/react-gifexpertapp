43ced365404a4fa09349d17d4141112ec10f045d
Alexis Guerrero <alexu.gro@gmail.com>
2020-09-10 19:19:26 -0500

    Docs agregado

---
+++ docs/precache-manifest.f0fc12ec87be000fe9b930f37792d54b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18ade5f2ebae0f1b264568464e4098a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33b20053763afaca7300",
+    "url": "/static/css/main.d3060a15.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33b20053763afaca7300",
+    "url": "/static/js/main.ad7c02db.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
