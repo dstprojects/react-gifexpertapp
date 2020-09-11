b289642d43071705fc558c81b51a951897eca050
Alexis Guerrero <alexu.gro@gmail.com>
2020-09-10 19:18:22 -0500

    First commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,29 @@
+import React, {useState} from 'react'
+import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
+
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['One punch']);    
+
+    return (
+        <>
+            <h2>
+                Gif Expert App
+            </h2>
+            <AddCategory setCategories={ setCategories } />
+            <hr></hr>
+            
+            <ol>
+                {
+                    categories.map( category =>
+                        <GifGrid 
+                            key={category}
+                            category={category}
+                        />
+                    )
+                }
+            </ol>
+        </>
+    )
+}
